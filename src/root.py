7f44305e3f82fa1231e979b8e716ea7823d87569
Laura Sans <60480966+laura-biolib@users.noreply.github.com>
2021-08-18 12:13:09 +0200

    change python script to accept trees also, not only aln

--- src/root.py
+++ src/root.py
@@ -31,23 +31,44 @@ def generate_tree(infile):
     print('![picture](tree.png)')
 
 
+def print_tree(infile, format):
+    # Read the alignment
+    tree = Phylo.read(infile, format)
+    fig = plt.figure(figsize = (12, 6))
+    ax = fig.add_subplot(111)
+    plt.title("Phylogenetic tree")
+    tree.ladderize() # Flip branches so deeper clades are displayed at top
+    dp = 5
+    Phylo.draw(tree, axes = ax, branch_labels = lambda c: round(c.branch_length, dp), do_show = False)
+    # Write output
+    plt.savefig("tree.png")
+    # Generate the Markdown preview
+    print('![picture](tree.png)')
+
 # Load input data
 parser = argparse.ArgumentParser()
+parser.add_argument("--input_type", default= "aln")
+parser.add_argument("--format_tree")
 parser.add_argument('--file', help = 'align', dest = 'file')
 parser.add_argument('--tree_type', help = 'Tree type Neighbor Joining or UPGMA', dest = 'tree_type')
 args = parser.parse_args()
 
-# Check the length of the input file
-counter = 0
-with open(args.file) as file:
-    for line in file:
-        if line.startswith('>'):
-            counter += 1
 
-if counter < 3:
-    print("There is too few sequences in your file. Please check your input has the correct length and format.")
-    sys.exit(1)
+if args.input_type == "aln":
+    # Check the length of the input file
+    counter = 0
+    with open(args.file) as file:
+        for line in file:
+            if line.startswith('>'):
+                counter += 1
 
-# Call MUSCLE
+    if counter < 3:
+        print("There is too few sequences in your file. Please check your input has the correct length and format.")
+        sys.exit(1)
 
-generate_tree(args.file)
+    # generate tree
+    generate_tree(args.file)
+elif args.input_type == "tree":
+    # print tree
+    print_tree(args.file, args.format_tree)
+
